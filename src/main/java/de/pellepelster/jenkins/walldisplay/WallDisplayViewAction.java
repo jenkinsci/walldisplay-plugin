ecbebc0d50353bcdb452b785c13998727375fc63
Joakim Sandström <fredrik.joakim.sandstrom@gmail.com>
2012-09-14 00:04:30 +0300

    fixed code format

--- src/main/java/de/pellepelster/jenkins/walldisplay/WallDisplayViewAction.java
+++ src/main/java/de/pellepelster/jenkins/walldisplay/WallDisplayViewAction.java
@@ -13,39 +13,39 @@ import java.net.URLEncoder;
  */
 public class WallDisplayViewAction implements Action {
 
-	private static final String ENCODING = "UTF-8";
-	private String viewName;
+    private static final String ENCODING = "UTF-8";
+    private String viewName;
 
-	public WallDisplayViewAction(String viewName) {
-		this.viewName = viewName;
+    public WallDisplayViewAction(String viewName) {
+	this.viewName = viewName;
+    }
+
+    public String getIconFileName() {
+	return "/plugin/jenkinswalldisplay/images/icon.png";
+    }
+
+    public String getDisplayName() {
+	return "Wall Display";
+    }
+
+    public String getUrlName() {
+
+	String encodedUrl = null;
+	String encodedViewName = null;
+	String rootUrl = getRootUrl();
+	try {
+	    encodedUrl = URLEncoder.encode(rootUrl, ENCODING);
+	    encodedViewName = URLEncoder.encode(viewName, ENCODING);
+	} catch (UnsupportedEncodingException e) {
+	    encodedUrl = rootUrl;
+	    encodedViewName = viewName;
 	}
+	return String
+		.format("%s/plugin/jenkinswalldisplay/walldisplay.html?viewName=%s&jenkinsUrl=%s",
+			rootUrl, encodedViewName, encodedUrl);
+    }
 
-	public String getIconFileName() {
-		return "/plugin/jenkinswalldisplay/images/icon.png";
-	}
-
-	public String getDisplayName() {
-		return "Wall Display";
-	}
-
-	public String getUrlName() {
-
-		String encodedUrl = null;
-		String encodedViewName = null;
-		String rootUrl = getRootUrl();
-		try {
-			encodedUrl = URLEncoder.encode(rootUrl, ENCODING);
-			encodedViewName = URLEncoder.encode(viewName, ENCODING);
-		} catch (UnsupportedEncodingException e) {
-			encodedUrl = rootUrl;
-			encodedViewName = viewName;
-		}
-		return String
-				.format("%s/plugin/jenkinswalldisplay/walldisplay.html?viewName=%s&jenkinsUrl=%s",
-						rootUrl, encodedViewName, encodedUrl);
-	}
-
-	protected String getRootUrl() {
-		return Hudson.getInstance().getRootUrl();
-	}
+    protected String getRootUrl() {
+	return Hudson.getInstance().getRootUrl();
+    }
 }
--- src/test/java/de/pellepelster/jenkins/walldisplay/WallDisplayViewActionTest.java
+++ src/test/java/de/pellepelster/jenkins/walldisplay/WallDisplayViewActionTest.java
@@ -17,52 +17,52 @@ import org.jvnet.hudson.test.HudsonTestCase;
  */
 public class WallDisplayViewActionTest extends HudsonTestCase {
 
-	private String rootUrl;
-	private String encodedRootUrl;
+    private String rootUrl;
+    private String encodedRootUrl;
 
-	/**
-	 * Instantiates a new WallDisplayViewAction with a stubbed root URL
-	 * 
-	 * @param viewName
-	 *            view name during test
-	 * @return a new WallDisplayViewAction instance
-	 * @throws IOException
-	 */
-	public WallDisplayViewAction newAction(String viewName) throws IOException {
-		WallDisplayViewAction action = spy(new WallDisplayViewAction(viewName));
-		rootUrl = getURL().toExternalForm();
-		encodedRootUrl = URLEncoder.encode(rootUrl, "UTF-8");
-		doReturn(rootUrl).when(action).getRootUrl();
-		return action;
-	}
+    /**
+     * Instantiates a new WallDisplayViewAction with a stubbed root URL
+     * 
+     * @param viewName
+     *            view name during test
+     * @return a new WallDisplayViewAction instance
+     * @throws IOException
+     */
+    public WallDisplayViewAction newAction(String viewName) throws IOException {
+	WallDisplayViewAction action = spy(new WallDisplayViewAction(viewName));
+	rootUrl = getURL().toExternalForm();
+	encodedRootUrl = URLEncoder.encode(rootUrl, "UTF-8");
+	doReturn(rootUrl).when(action).getRootUrl();
+	return action;
+    }
 
-	/**
-	 * Verifies that the wall-display link for the 'All' view name is correct.
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void testGetUrlNameAllView() throws IOException {
-		WallDisplayViewAction action = newAction("All");
-		assertEquals(
-				rootUrl
-						+ "/plugin/jenkinswalldisplay/walldisplay.html?viewName=All&jenkinsUrl="
-						+ encodedRootUrl, action.getUrlName());
-	}
+    /**
+     * Verifies that the wall-display link for the 'All' view name is correct.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testGetUrlNameAllView() throws IOException {
+	WallDisplayViewAction action = newAction("All");
+	assertEquals(
+		rootUrl
+			+ "/plugin/jenkinswalldisplay/walldisplay.html?viewName=All&jenkinsUrl="
+			+ encodedRootUrl, action.getUrlName());
+    }
 
-	/**
-	 * Verifies that the wall-display link for a view with special characters is
-	 * encoded correctly.
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void testGetUrlNameEncodedView() throws IOException {
-		WallDisplayViewAction action = newAction("+Dashboard");
-		assertEquals(
-				rootUrl
-						+ "/plugin/jenkinswalldisplay/walldisplay.html?viewName=%2BDashboard&jenkinsUrl="
-						+ encodedRootUrl, action.getUrlName());
-	}
+    /**
+     * Verifies that the wall-display link for a view with special characters is
+     * encoded correctly.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testGetUrlNameEncodedView() throws IOException {
+	WallDisplayViewAction action = newAction("+Dashboard");
+	assertEquals(
+		rootUrl
+			+ "/plugin/jenkinswalldisplay/walldisplay.html?viewName=%2BDashboard&jenkinsUrl="
+			+ encodedRootUrl, action.getUrlName());
+    }
 
 }
