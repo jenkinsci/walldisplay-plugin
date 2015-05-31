ea265bc6ac48485d36966ceaefe010594a77e690
Andy Pemberton <pembertona@gmail.com>
2015-05-30 22:46:39 -0400

    JENKINS-26036 additionally, remove nested-view dependency

--- src/main/java/de/pellepelster/jenkins/walldisplay/WallDisplayTransientViewActionFactory.java
+++ src/main/java/de/pellepelster/jenkins/walldisplay/WallDisplayTransientViewActionFactory.java
@@ -2,15 +2,10 @@ package de.pellepelster.jenkins.walldisplay;
 
 import hudson.model.Action;
 import hudson.model.TransientViewActionFactory;
-import hudson.model.ViewGroup;
-import hudson.model.Hudson;
 import hudson.model.View;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
-
-import org.apache.commons.lang.StringUtils;
 
 /**
  * Adds the walldisplay link action to all views
@@ -21,24 +16,8 @@ public class WallDisplayTransientViewActionFactory extends TransientViewActionFa
 
     @Override
     public List<Action> createFor(final View view) {
-
-        List<Action> result = new ArrayList<Action>();
-        String viewName = view.getViewName();
-
-        if (Hudson.getInstance().getPlugin("nested-view") != null) {
-            ArrayList<String> viewNames = new ArrayList<String>();
-            viewNames.add(viewName);
-            ViewGroup owner = view.getOwner();
-            while (owner instanceof hudson.plugins.nested_view.NestedView) {
-                viewNames.add(((hudson.plugins.nested_view.NestedView) owner).getViewName());
-                owner = ((hudson.plugins.nested_view.NestedView) owner).getOwner();
-            }
-            Collections.reverse(viewNames);
-            viewName = StringUtils.join(viewNames, "/view/");
-        }
-        result.add(new WallDisplayViewAction(viewName, view.getOwner().getUrl()));
-        return result;
-
+        WallDisplayViewAction action = new WallDisplayViewAction(view.getViewName(), view.getOwner().getUrl());
+        return Arrays.asList((Action) action);
     }
 
 }
